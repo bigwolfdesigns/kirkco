61e904bbe5baa9bf9a38f0b87f4493b93f1719f4
Billy Stalnaker <billy@bigwolfdesigns.com>
2015-02-03 21:28:16 -0500

    Project Properties and such, hopefully this will work

---
+++ index.php
@@ -0,0 +1,128 @@
+<?php
+
+
+	$index = "yes";
+	//$level = '';
+	//$pagename = 'Industrial Adhesive and Sealant Dispensing by Kirkco';
+	//$pagetitle = $pagename . '';
+
+	include("header.php");
+
+
+//echo '<div align="center" style="border:1px solid #CCCCCC;background-color:#e8e8e8;margin:0px;padding:10px; font-size:12px;margin-bottom:10px;">';
+//include('/home/kirkcoco/public_html/aps-mini-selector.php'); 
+//echo '</div>';
+ 
+//echo $featured_products;
+
+?>
+ 
+<img alt="" src="images/learn-more-home.jpg" width="920" height="30" /><div style="clear:both"></div>
+
+<script type="text/javascript">
+function JumpToIt(list) 
+	{
+	var newPage = list.options[list.selectedIndex].value
+	if (newPage != "None") {location.href=newPage}
+	}
+</script>
+
+<table width="920" border="0" cellspacing="0" cellpadding="5">
+	<tr valign="top">
+	<td width="261"><span class="bucket-left"><span class="product-header"><br />
+	<a href="products/two-component-systems/">Two Component Systems</a></span><span class="drop-down"><form>
+	<select width="20" onchange="JumpToIt(this)">
+	<option value="None">\\ Select a Product</option>
+	<option value="None">--------------------------------------------------------</option>
+	<option value="products/two-component-systems/meter-mix-supply-units/">Meter/Mix Supply Units</option>
+	<option value="products/two-component-systems/meter-mix-supply-units/micro-mix/">Micro-Mix</option>
+	<option value="products/two-component-systems/meter-mix-supply-units/econo-mix/">Econo-Mix</option>
+	<option value="products/two-component-systems/meter-mix-supply-units/eldo-mix/">Eldo-Mix</option>
+	<option value="products/two-component-systems/meter-mix-supply-units/vario-mix/">Vario-Mix</option>
+	<option value="products/two-component-systems/meter-mix-supply-units/silco-mix/">Silco-Mix</option>
+	<option value="products/two-component-systems/meter-mix-supply-units/graco-hydra-cat/">Graco Hydra Cat</option>
+	<option value="products/two-component-systems/meter-mix-supply-units/graco-8900/">Graco 8900</option>
+	<option value="products/two-component-systems/mixing-valves/"> Mixing Valves</option>
+	</select>
+	</form></span>
+	<img alt="Two Component Systems" src="images/home-two-component-systems(gpmt92).gif" width="85" height="85" class="home-image" /><p>Engineered systems for the processing and production operations where multiple component elastomer materials must be metered, mixed, and dispensed with precision. </p></span></td>
+	<td width="362"><span class="bucket-center"><span class="product-header"><br /><a href="products/hot-melt-adhesive-technology/">Hot Melt Adhesive Technology</a></span><span class="drop-down"><form>
+	<select width="20" onchange="JumpToIt(this)">
+	<option value="None">\\ Select a Product</option>
+	<option value="None">--------------------------------------------------------</option>
+	<option value="products/hot-melt-adhesive-technology/automatic-hot-melt-applicators/">Automatic Hot Melt Applicators</option>
+	<option value="products/hot-melt-adhesive-technology/automatic-hot-melt-applicators/itw/">ITW</option>
+	<option value="products/hot-melt-adhesive-technology/automatic-hot-melt-applicators/itw-ufd-lpt/">ITW, UFD, LPT</option>
+	<option value="products/hot-melt-adhesive-technology/manual-applicators/">Manual Applicators</option>
+	<option value="products/hot-melt-adhesive-technology/roll-coaters-laminators/">Roll Coaters/Laminators</option>
+	<option value="products/hot-melt-adhesive-technology/bulk-units/">Bulk Units</option>
+	<option value="products/hot-melt-adhesive-technology/bulk-units/itw-du55-convert-a-drum/">ITW, DU55, Convert a Drum</option>
+	<option value="products/hot-melt-adhesive-technology/bulk-units/graco-therm-o-flow-piston-pumps/">Graco, Therm-O-Flow Piston Pumps</option>
+	<option value="products/hot-melt-adhesive-technology/bulk-units/itw-m-series/">ITW, M Series</option>
+	<option value="products/hot-melt-adhesive-technology/bulk-units/pail-units/">Pail Units</option>
+	<option value="products/hot-melt-adhesive-technology/reactive-hot-melt-systems/">Reactive Hot Melt Systems</option>
+	<option value="products/hot-melt-adhesive-technology/reactive-hot-melt-systems/itw-aps-unit/">ITW, APS Unit</option>
+	<option value="products/hot-melt-adhesive-technology/bulk-units/graco-therm-o-flow-piston-pumps/">Drum/Pail Systems Therm-O-Flow 20/200</option>
+	<option value="products/hot-melt-adhesive-technology/reactive-hot-melt-systems_/drum-pail-systems-rmc-20-200/">Drum/Pail Systems RMC 20 / RMC 200</option>
+	<option value="products/hot-melt-adhesive-technology/tank-units/">Tank Units</option>
+	<option value="products/hot-melt-adhesive-technology/tank-units/small-tank-hotmelt-application-systems-graco-t5-t7-and-t18/">Small Tank Hotmelt Application Systems Graco </option>
+	<option value="products/hot-melt-adhesive-technology/tank-units/small-tank-hotmelt-application-systems-graco-t5-t7-and-t18/">T5, T7, and T18</option>
+	<option value="products/hot-melt-adhesive-technology/tank-units/itw-dynamini-dynamelt/">ITW: Dynamini, Dynamelt</option>
+	<option value="products/hot-melt-adhesive-technology/tank-units/dds/">DDS</option>
+	</select>
+	</form></span><img alt="Hot Melt Adhesive Technology" src="images/home-hot-melt-adhesive-technology(uhqt92).gif" width="85" height="85" class="home-image" /><p>From stick and cartridge handguns to bulk dispensing systems for thermoplastic and PUR hot melt adhesives. We have an adhesive supply unit to meet your needs.</p></span></td>
+	<td width="257"><span class="bucket-right"><span class="product-header"><br />
+	<a href="products/single-component-systems/">Single Component Systems</a></span><span class="drop-down"><form>
+	<select width="20" onchange="JumpToIt(this)">
+	<option value="None">\\ Select a Product</option>
+	<option value="None">--------------------------------------------------------</option>
+	<option value="products/single-component-systems/metering-valves/">Metering Valves</option>
+	<option value="products/single-component-systems/dispensing-valves/">Dispensing Valves</option>
+	<option value="products/single-component-systems/pump-systems/">Pump Systems</option>
+	<option value="products/single-component-systems/fluid-regulating-valves/">Fluid Regulating Valves</option>
+	<option value="products/single-component-systems/electronically-controlled-metering-systems/">Electronically Controlled Metering Systems</option>
+	<option value="products/single-component-systems/electronically-controlled-metering-systems/precision-swirl/">Precision Swirl</option>
+	<option value="products/single-component-systems/electronically-controlled-metering-systems/precision-liquid-applied-sound-deadening-system/">Precision Liquid-Applied Sound Deadening System</option>
+	<option value="products/single-component-systems/electronically-controlled-metering-systems/precision-flo-xl-lt-fluid-metering-system/">Precision Flo-XL/LT Fluid Metering System</option>
+	</select>
+	</form></span><img alt="Single Component Systems" src="images/home-single-component-systems(ykqt92).gif" width="85" height="85" class="home-image" /><p>Pumps and valves suitable for processing a wide range of materials and viscosities. Capable of metering a precise quantity, laying beads, or continuous high volume pours. </p></span></td>
+	</tr>
+	<tr valign="top">
+	<td><span class="bucket-left"><span class="product-header"><br /><a href="products/spraying-and-coating/">Spraying and Coating</a></span><span class="drop-down"><form>
+	<select width="20" onchange="JumpToIt(this)">
+	<option value="None">\\ Select a Product</option>
+	<option value="None">--------------------------------------------------------</option>
+	<option value="products/spraying-and-coating/material-tanks-and-circulation-systems/">Material Tanks &amp; Circulation Systems</option>
+	<option value="products/spraying-and-coating/pilot-automatic-spray-guns/">Pilot Automatic Spray Guns</option>
+	<option value="products/spraying-and-coating/pilot-manual-spray-guns/">Pilot Manual Spray Guns</option>
+	<option value="products/spraying-and-coating/coating-systems-for-2-part-water-based-adhesives/">Coating Systems for 2-Part Water Based Adhesives</option>
+	<option value="products/spraying-and-coating/reactor-polyurea-spray-application-system/">Reactor Polyurea Spray Application System</option>
+	<option value="products/spraying-and-coating/xtreme-mix/">Xtreme Mix</option>
+	<option value="products/spraying-and-coating/orion-finishing-pump/">Orion Finishing Pump</option>
+	<option value="products/spraying-and-coating/fusion-spray-gun/">Fusion Spray Gun</option>
+	</select>
+	</form></span><img alt="Spraying and Coating" src="images/home-spraying-and-coating(h5hh82).gif" width="85" height="85" class="home-image" /><p>High-quality products designed to improve productivity and reduce operating costs for your spray coating needs.</p></span></td>
+	<td><span class="bucket-center"><span class="product-header"><br /><a href="products/cold-glue-adhesive-systems/">Cold Glue Adhesive Systems</a></span><span class="drop-down"><form>
+	<select width="20" onchange="JumpToIt(this)">
+	<option value="None">\\ Select a Product</option>
+	<option value="None">--------------------------------------------------------</option>
+	<option value="products/cold-glue-adhesive-systems/cold-glue-applicators/">Cold Glue Automatic Applicators</option>
+	<option value="products/spraying-and-coating/coating-systems-for-2-part-water-based-adhesives/">Pilot Ratiomaster</option>
+	<option value="products/spraying-and-coating/pilot-automatic-spray-guns/">Pilot Automatic Spray Guns</option>
+	<option value="products/spraying-and-coating/pilot-automatic-spray-guns/">Pilot Manual Applicators</option>
+	<option value="products/cold-glue-adhesive-systems/fluid-delivery-and-pumping-systems/">Fluid Delivery &amp; Pumping Systems</option>
+	</select>
+	</form></span><img alt="Cold Glue Adhesive Systems" src="images/cold-glue-adhesive-system(i3yt92).gif" width="74" height="74" class="home-image" /><p>Proven line of high quality, cold liquid adhesive, pumping and dispensing systems.</p></span></td>
+	<td><!--<span class="product-header"><a href="events/">Upcoming Events</a></span>
+		<div style="clear:both"></div>
+	<p style="margin-top:15px;"><a href="events/"><image src="images/sspc2014.jpeg" width="200px" /></a></p>
+	</span>-->
+	</td>
+	</tr>
+	<tr>
+	<td colspan="3"></td>
+	</tr>
+</table>
+
+
+<?php include("footer.php"); ?>
